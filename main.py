9f263ff038293e92217d2572e1b5e5f43b79859c
SalilSavarin <yan-sav@yandex.ru>
2022-08-28 16:21:45 +0300

    Add requirements

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import application
 import datetime 
+from bs4 import BeautifulSoup as BS
+import requests
+from pprint import pprint
 
 
 
+url = 'https://stopgame.ru/games/PC/best?year_start=2021&year_end=2021'
+request = requests.get(url)
+soup = BS(request.text, 'html.parser')
+game_names = soup.find_all('div', class_= 'caption caption-bold')
+for name in game_names:
+    g_name = name.text.strip()
+    print(g_name)
+    
 if __name__ == '__main__':
     date_now = datetime.date.today()
     print(date_now)
